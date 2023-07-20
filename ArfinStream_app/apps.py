6bf13c1c2b286ca8fa9f0e68ec93d646fb9e0ab7
Arfinrabby30 <arfinrabby30@gmail.com>
2023-07-20 13:24:04 +0600

    first commit

---
+++ ArfinStream_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ArfinstreamAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ArfinStream_app'
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ArfinStream.wsgi --log-file -
